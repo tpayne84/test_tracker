0d747dc8216bbb322d34d5a947c199415c500a77
tpayne84 <Travis.Payne84@me.com>
2013-10-04 12:51:44 -0500

    Add provide title to index pages, update application layout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,24 +2,4 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  helper_method :badge_expired, :badge_soon, :badge_good
-
-
-  def badge_expired
-  	rec = Record.where("pass = ? AND requal_date < ?", true, Date.today)
-  	@badge_expired = rec.length
-  end
-
-  def badge_soon
-  	rec = Record.where("pass = ? AND requal_date < ? AND requal_date > ?", true, Date.today + 3.months, Date.today)
-  	@badge_soon = rec.length
-  end
-
-  def badge_good
-  	rec = Record.where("pass = ? AND requal_date > ?", true, Date.today + 3.months)
-  	@badge_good = rec.length
-  end
-
-
-
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,28 @@
 module ApplicationHelper
+
+
+	def full_title(page_title)
+		base_title = 'Records Tracker'
+		if page_title.empty?
+		  base_title
+		else
+		  "#{base_title} | #{page_title}"
+		end
+	end
+
+	def badge_expired
+		rec = Record.where("pass = ? AND requal_date < ?", true, Date.today)
+		@badge_expired = rec.length
+	end
+
+	def badge_soon
+		rec = Record.where("pass = ? AND requal_date < ? AND requal_date > ?", true, Date.today + 3.months, Date.today)
+		@badge_soon = rec.length
+	end
+
+	def badge_good
+		rec = Record.where("pass = ? AND requal_date > ?", true, Date.today + 3.months)
+		@badge_good = rec.length
+	end
+
 end
