efd69a72d25dcd17bf0511470650a121fbfd547f
tpayne84 <Travis.Payne84@me.com>
2013-10-05 11:00:24 -0500

    Updates look for tables and the show,edit, delete buttons on them... also removed lingering rails generated headers

--- app/models/record.rb
+++ app/models/record.rb
@@ -5,4 +5,7 @@ class Record < ActiveRecord::Base
 	has_one :group, through: :user
 
 	scope :by_client, ->(id) { where(:client_id => id)}
+	scope :by_group, ->(id) { where(:group_id => id)}
+	scope :by_user, ->(id) { where(:user_id => id)}
+
 end
