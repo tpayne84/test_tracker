9f8fa46d38d9d31ca3065197a8f26e33bc499ef5
tpayne84 <Travis.Payne84@me.com>
2013-10-06 09:20:08 -0500

    fix a few form view css items

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131005032724) do
+ActiveRecord::Schema.define(version: 20131006133746) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -75,6 +75,24 @@ ActiveRecord::Schema.define(version: 20131005032724) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "group_id"
+    t.string   "encrypted_password",   limit: 128, default: "", null: false
+    t.string   "password_salt"
+    t.string   "authentication_token"
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string   "reset_password_token"
+    t.string   "remember_token"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",                    default: 0
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.integer  "failed_attempts",                  default: 0
+    t.string   "unlock_token"
+    t.datetime "locked_at"
+    t.string   "email"
   end
 
 end
