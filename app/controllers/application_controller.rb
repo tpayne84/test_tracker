908dc98530c066437977b9dc30a68757ce59c7a2
tpayne84 <Travis.Payne84@me.com>
2013-10-16 08:16:31 -0500

    Fixed error on activity report and added get_course_name and get_course_min_score to application controller helper methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,15 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  helper_method :get_course_name, :get_course_min_score
+
+  def get_course_min_score(course_id)
+    @get_course_min_score = Course.find(course_id)
+    @get_course_min_score = @get_course_min_score.min_score
+  end
+
+  def get_course_name(course_id)
+    @get_course_name = Course.find(course_id)
+    @get_course_name = @get_course_name.name
+  end
 end
--- app/controllers/status_report_controller.rb
+++ app/controllers/status_report_controller.rb
@@ -14,15 +14,5 @@ class StatusReportController < ApplicationController
         @records = Record.page(params[:page]).per_page(10)
       end
 	end
-
-  def get_course_name(course_id)
-    @get_course_name = Course.find(course_id)
-    @get_course_name = @get_course_name.name
-  end
-
-  def get_course_min_score(course_id)
-    @get_course_min_score = Course.find(course_id)
-    @get_course_min_score = @get_course_min_score.min_score
-  end
 	
 end
